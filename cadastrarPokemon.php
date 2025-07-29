eeb5c9cda713b9954abbc267c7b4dca6b14ecf4f
nicole <nicolecafalloni9@gmail.com>
2025-07-29 09:38:04 -0300

    alterações nicole

--- cadastrarPokemon.php
+++ cadastrarPokemon.php
@@ -8,6 +8,11 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Bitcount:wght@100..900&family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Bitcount:wght@100..900&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+
+
 </head>
 <body>
     <div class="container">
@@ -18,7 +23,7 @@
 
     <div class="alinha-card">
         <div class="card">
-
+            <p> oi</p>
         </div>
     </div>
 </body>
