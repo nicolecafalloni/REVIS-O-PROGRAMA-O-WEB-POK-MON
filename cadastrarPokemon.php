6b5e82d076988f32dbdb4bc94056b3c08590dc0f
nicole <nicolecafalloni9@gmail.com>
2025-07-29 09:28:56 -0300

    criação cadastrar pokémon

---
+++ cadastrarPokemon.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar Pokémon</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Bitcount:wght@100..900&family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <h1 class="text-principal">
+            CADASTRAR POKÉMON
+        </h1>
+    </div>
+
+    <div class="alinha-card">
+        <div class="card">
+
+        </div>
+    </div>
+</body>
+</html>
